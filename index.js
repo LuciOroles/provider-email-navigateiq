6876eb2b192c6123332b26f06a93d37ad03f3a06
Lucian <stoica.florin.lucian@gmail.com>
2025-01-03 20:31:51 +0200

    add minimal implementation

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const { setTimeout } = require('timers/promises');
+const nodemailer = require('nodemailer');
 
 module.exports = {
     init: (providerOptions = {}, settings = {}) => {
-            // console.log(providerOptions, settings)
-            console.log(`init:project`);
+        const { username, emailPassword, hostName, hostAlternatePorts } = providerOptions;
+        const { defaultFrom } = settings;
+        const transport = nodemailer.createTransport({
+            host: hostName,
+            port: hostAlternatePorts,
+            tls: {
+                ciphers: 'SSLv3'
+            },
+            secure: false,
+            auth: {
+                type: 'LOGIN',
+                user: username,
+                pass: emailPassword,
+            }
+        });
+
         return {
+            /**
+             * 
+             * @param {Object} options
+             *{ to: any;
+                from: string | undefined;
+                replyTo: any;
+                subject: string;
+                text: string;
+                html: string;} 
+             * @returns {boolean}
+             */
             send: async (options) => {
-                await setTimeout(300);
-                console.log(`sending the email`);
+                const { to, subject, text, html } = options;
+
+                await transport.sendMail({
+                    from: defaultFrom,
+                    to,
+                    subject,
+                    text,
+                    html,
+                });
+             
                 return true;
             }
         }
